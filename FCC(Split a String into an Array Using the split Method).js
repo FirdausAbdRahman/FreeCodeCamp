0bcca8b195e81d6384f13031a3bc76b9205752fd
Mona Ka <monaka@outlook.com>
2018-09-19 13:28:38 -0500

    Functional Programming practice

---
+++ FCC(Split a String into an Array Using the split Method).js
@@ -0,0 +1,9 @@
+function splitify(str) {
+    // Add your code below this line
+    console.log(str.split(/\W+/));
+    return str.split(/\W+/);
+
+
+    // Add your code above this line
+}
+splitify("Hello World,I-am code");
