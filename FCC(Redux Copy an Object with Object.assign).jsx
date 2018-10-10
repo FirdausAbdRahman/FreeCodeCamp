4d142f1ddcbebb78549b3a3658e9061f68e29fab
Mona Ka <monaka@outlook.com>
2018-10-10 11:02:23 -0500

    Redux: Copy an Object with Object.assign
    Free Code Camp Redux Practice

---
+++ FCC(Redux Copy an Object with Object.assign).jsx
@@ -0,0 +1,26 @@
+const defaultState = {
+    user: 'CamperBot',
+    status: 'offline',
+    friends: '732,982',
+    community: 'freeCodeCamp'
+  };
+  
+  const immutableReducer = (state = defaultState, action) => {
+    switch(action.type) {
+      case 'ONLINE':
+        // don't mutate state here or the tests will fail
+        console.log(action.type.toLowerCase());
+        return Object.assign({}, state, {status: action.type.toLowerCase()});
+        
+      default:
+        return state;
+    }
+  };
+  
+  const wakeUp = () => {
+    return {
+      type: 'ONLINE'
+    }
+  };
+  
+  const store = Redux.createStore(immutableReducer);
