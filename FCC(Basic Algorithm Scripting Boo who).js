b1db5c38486fec474995ff31670b1d2ada1a33c0
Mona Ka <monaka@outlook.com>
2018-10-15 13:34:16 -0500

    Basic Algorithm Scripting: Boo who
    Free Code Camp Practice

---
+++ FCC(Basic Algorithm Scripting Boo who).js
@@ -0,0 +1,12 @@
+function booWho(bool) {
+    // What is the new fad diet for ghost developers? The Boolean.
+    console.log(typeof(bool));
+    if (typeof bool === 'boolean') {
+        return true;
+    } else {
+        return false;
+    }
+    //return bool;
+}
+
+booWho(null);
