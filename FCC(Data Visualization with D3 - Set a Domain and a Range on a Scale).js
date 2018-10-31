a577e40eecc95f075ae9980ca537dd81cb6415ef
Mona Ka <monaka@outlook.com>
2018-10-31 12:01:29 -0500

    Data Visualization with D3 - Set a Domain and a Range on a Scale)
    Free Code Camp Practice

---
+++ FCC(Data Visualization with D3 - Set a Domain and a Range on a Scale).js
@@ -0,0 +1,12 @@
+//Data Visualization with D3: Add Labels to Scatter Plot Circles
+
+// Add your code below this line
+const scale = d3.scaleLinear();
+scale.domain([250, 500]);
+scale.range([10, 150]);
+
+// Add your code above this line
+const output = scale(50);
+d3.select("body")
+    .append("h2")
+    .text(output);
